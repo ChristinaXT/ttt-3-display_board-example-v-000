72ad9d153b8a29587b9783ec3c41ddfdd61e6677
Christina Williams <christinaxtwilliams@gmail.com>
2018-07-25 16:15:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 def display_board 
   separator = "|"
   lines = "-----------"
-  puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} " 
-  puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
-  puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
+  print " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} " 
+  print " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
+  print " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
 end
