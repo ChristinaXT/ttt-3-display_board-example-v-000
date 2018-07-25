9064ee0e44d5006f9dd9fe93722982f22673185b
Christina Williams <christinaxtwilliams@gmail.com>
2018-07-25 16:20:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,9 @@ def display_board
   separator = "|"
   lines = "-----------"
   puts "A Tic Tac Toe Board"
-  print " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} " 
-  print " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
-  print " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
+  puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+  puts "#{lines}"
+  puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
+  puts "#{lines}"
+  puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
 end
